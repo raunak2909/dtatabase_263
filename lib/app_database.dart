6397d9d658d6fa269151af1879a98352fda13edc
raunakpurohit <raunak.wscubetech@gmail.com>
2024-06-10 21:12:10 +0530

    Database implemented via Bottom sheet and snack bar

--- lib/app_database.dart
+++ lib/app_database.dart
@@ -1,52 +1,80 @@
+import 'package:dtatabase_263/main.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
-class AppDatabase{
+class AppDatabase {
   //Private Constructor (Singleton class creation)
   AppDatabase._();
 
   static final AppDatabase db = AppDatabase._();
+
+  static const String DBNAME = "notes.db";
+  static const String TABLE_NOTE = "noted";
+  static const String COLUMN_NOTE_ID = "n_id";
+  static const String COLUMN_NOTE_TITLE = "n_title";
+  static const String COLUMN_NOTE_DESC = "n_desc";
 
   //1
   Database? mDB;
 
   //2
   //getDB
-  Future<Database> getDB() async{
-
-    if(mDB!=null){
+  Future<Database> getDB() async {
+    if (mDB != null) {
       return mDB!;
     } else {
       //openDB
       return await openDB();
     }
-
   }
 
-  Future<Database> openDB() async{
-
+  Future<Database> openDB() async {
     var rootPath = await getApplicationDocumentsDirectory();
     // data/data/com.example.db/databases/notes.db;
-    var dbPath = join(rootPath.path, "notes.db");
+    var dbPath = join(rootPath.path, DBNAME);
 
     //openDB
     return await openDatabase(dbPath, version: 1,
         //createDB
-        onCreate: (db, version){
+        onCreate: (db, version) {
       //will be creating all the tables required to maintain in db
       //Tables
 
       /// run any sql query
-      db.execute("create Table noted (n_id integer primary key autoincrement, n_title text, n_desc text)");
-
+      db.execute(
+          "create table $TABLE_NOTE ($COLUMN_NOTE_ID integer primary key autoincrement, $COLUMN_NOTE_TITLE text, $COLUMN_NOTE_DESC text)");
     });
-
   }
 
+  ///queries
 
+  //create(insert)
+  Future<bool> insertNote({required String title, required String desc}) async{
 
+    var mainDB = await getDB();
 
+    int rowsEffect = await mainDB.insert(TABLE_NOTE, {
+      COLUMN_NOTE_TITLE : title,
+      COLUMN_NOTE_DESC : desc,
+    });
 
+    return rowsEffect>0;
+  }
+
+  //read(fetch)
+  Future<List<Map<String, dynamic>>> fetchAllNotes() async{
+
+    var mainDB = await getDB();
+    List<Map<String, dynamic>> mNotes = [];
+
+    try {
+      mNotes = await mainDB.query(TABLE_NOTE);
+    } catch(e){
+      print("Error: ${e.toString()}");
+    }
+
+    return mNotes;
+  }
 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -22,11 +22,164 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Map<String, dynamic>> allNotes = [];
+  var titleController = TextEditingController();
+  var descController = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getAllNotes();
+  }
+
+  void getAllNotes() async {
+    allNotes = await AppDatabase.db.fetchAllNotes();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     AppDatabase db = AppDatabase.db;
-    return Scaffold();
-  }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notes'),
+      ),
+      body: allNotes.isNotEmpty
+          ? ListView.builder(
+              itemCount: allNotes.length,
+              itemBuilder: (_, index) {
+                return ListTile(
+                  title: Text(allNotes[index][AppDatabase.COLUMN_NOTE_TITLE]),
+                  subtitle: Text(allNotes[index][AppDatabase.COLUMN_NOTE_DESC]),
+                );
+              })
+          : Center(
+              child: Text('No Notes yet!!'),
+            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          showModalBottomSheet(
+              context: context,
+              builder: (_) {
+                return Container(
+                  height: 400,
+                  decoration: BoxDecoration(
+                    borderRadius:
+                        BorderRadius.vertical(top: Radius.circular(43)),
+                    color: Colors.grey.shade200,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(11.0),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Add Note',
+                          style: TextStyle(
+                              fontSize: 25, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          'Add you brief title and detailed note here',
+                          style: TextStyle(fontSize: 12),
+                        ),
+                        SizedBox(
+                          height: 11,
+                        ),
+                        TextField(
+                          controller: titleController,
+                          decoration: InputDecoration(
+                              fillColor: Colors.white,
+                              filled: true,
+                              hintText: "Enter title in here..",
+                              label: Text("Title"),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.vertical(
+                                    top: Radius.circular(25)),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.vertical(
+                                    top: Radius.circular(25)),
+                              )),
+                        ),
+                        TextField(
+                          controller: descController,
+                          decoration: InputDecoration(
+                              fillColor: Colors.white,
+                              filled: true,
+                              hintText: "Enter desc in here..",
+                              label: Text("Desc"),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.vertical(
+                                    bottom: Radius.circular(25)),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.vertical(
+                                    bottom: Radius.circular(25)),
+                              )),
+                        ),
+                        SizedBox(
+                          height: 21,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            OutlinedButton(
+                                onPressed: () async{
+                                  if (titleController.text.isNotEmpty &&
+                                      descController.text.isNotEmpty) {
+                                    bool check = await AppDatabase.db.insertNote(
+                                        title: titleController.text,
+                                        desc: descController.text);
 
+                                    if(check) {
+                                      getAllNotes();
+                                      titleController.clear();
+                                      descController.clear();
+                                      Navigator.pop(context);
+                                    }
+
+
+
+                                  } else {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                        SnackBar(
+                                            content: Text(
+                                                'TItle or Desc fields cannot be empty, please fill up all the required blanks!!')));
+                                  }
+                                },
+                                child: Text('Add')),
+                            OutlinedButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Row(
+                                  children: [
+                                    FlutterLogo(),
+                                    Text('Cancel'),
+                                  ],
+                                ))
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              },
+              /*enableDrag: false,
+           isDismissible: false,*/ /* barrierColor: Colors.blue.shade100,*/
+              backgroundColor: Colors.blue.shade200,
+              shape: RoundedRectangleBorder(
+                  borderRadius:
+                      BorderRadius.vertical(top: Radius.circular(43))));
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
 }
